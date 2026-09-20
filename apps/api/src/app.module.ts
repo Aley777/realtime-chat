471f13920d8b6695342b5a541e6960b1fb727868
Aley777 <aleynaaydogd@gmail.com>
2026-09-20 20:54:01 +0300

    feat(api): setup prisma 7 with user model

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validateEnv } from './config/env.validation';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, validate: validateEnv })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
